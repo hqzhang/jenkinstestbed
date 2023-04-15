181ac5398d69c0924d69a385630f98b7369039cf
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-04-14 20:16:52 -0400

    update

---
+++ vars/menu.groovy
@@ -0,0 +1,24 @@
+def getFileContent(){
+    def mf ="ls /Users/hongqizhang/workspace/ansibletest/releases  ".execute().text
+    def myls = mf.readLines().collect{ it.split()[0].minus('.xml')}
+    def map=[:]
+    myls.each { file->
+            my_tag="curl -k https://raw.githubusercontent.com/hqzhang/ansibletest/main/releases/${file}.xml".execute().text 
+            map[ file]= my_tag
+    }
+    my_tag=map[SolutionDetail]
+    return """
+        <!--table><tr>
+        <tr>
+        <td class="setting-main"-->
+            <textarea name=\"value\"  value  class=\"setting-input  \" type=\"text\">${my_tag}</textarea>
+            <!--/td>
+        </tr>
+    </tr></table-->
+    """
+}
+
+def getFileList(){
+    def mf ="ls /Users/hongqizhang/workspace/ansibletest/releases  ".execute().text
+    return mf.readLines().collect{ it.split()[0].minus('.xml')}
+}

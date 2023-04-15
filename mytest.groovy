56fb20a27625f2270183c58d787ea7e447868e51
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-04-14 20:49:51 -0400

    update

--- mytest.groovy
+++ mytest.groovy
@@ -4,20 +4,34 @@
     import groovy.yaml.YamlSlurper
     import mylib
     import sayHello
-    @groovy.transform.Field
+  
+    String mybuildScript(List values){
+       def ret=values.collect { '"'+it+'"' }
+    return "return ${ret}"
+    }
+
+def getFileContent(String SolutionDetail ){
     def mf ="ls /Users/hongqizhang/workspace/ansibletest/releases  ".execute().text
     def myls = mf.readLines().collect{ it.split()[0].minus('.xml')}
     def map=[:]
     myls.each { file->
-        my_tag="curl -k https://raw.githubusercontent.com/hqzhang/ansibletest/main/releases/${file}.xml".execute().text 
-        map[ file]= my_tag
+            my_tag="curl -k https://raw.githubusercontent.com/hqzhang/ansibletest/main/releases/${file}.xml".execute().text 
+            map[ file]= my_tag
     }
-    myls.each { file->
-       
-        println map[file]
-    }
-    
-    
+    my_tag=map[SolutionDetail]
+    return """<textarea name=\"value\"  value  class=\"setting-input  \" type=\"text\">${my_tag}</textarea>"""
+}
+
+def getFileList(){
+    def test=''
+    def mf ="ls /Users/hongqizhang/workspace/ansibletest/releases  ".execute().text
+    def out=mf.readLines().collect{ it.split()[0].minus('.xml')}
+    mybuildScript(out)
+}
+println getFileList()
+
+println getFileContent('config')
+println """<textarea name=\"value\"  value  class=\"setting-input  \" type=\"text\">${my_tag}</textarea>"""
 
     System.exit(1)
     List myvar = ['abc','efd','xyz']

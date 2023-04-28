54d05a12db50095c6544b6dec019fa606c5d3fe4
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-04-27 23:33:18 -0400

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,11 @@
  
  set -x
  var="jenkins_home"
- cmd="ssh hongqizhang@localhost \"cd $var && pwd && ls -al .\""
+ cmd="ssh hongqizhang@localhost \'cd $var && pwd && ls -al .\'"
+ 
+ #ssh hongqizhang@localhost '"cd' jenkins_home '&&' pwd '&&' ls -al '."'
+ #ssh hongqizhang@localhost 'pwd && ls -al jenkins_home'
  log=`$cmd`
+ 
  echo "log=$log"
                       

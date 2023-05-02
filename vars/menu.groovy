01b18b5881ecfe2a3970cd4d5a7d6ab7fe356548
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-02 17:54:52 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -49,7 +49,7 @@ def getCompList(String mypack){
     |out.each{ if (it.contains(".tar.gz") ){
     |  if ( ! key.isEmpty()){ map[key]=lss; key=it; lss=[] } else { key=it } }
     |else { lss.add('"'+it+'"') } } ; map[key]=lss
-    |return \"\"\"\${map[${mypack}]} \"\"\"
+    |return \"\"\"return \${map[${mypack}]} \"\"\"
     | """.stripMargin()
 }
 def gitCompVerify(){
@@ -61,7 +61,7 @@ def out="ssh root@192.168.0.16 /root/workspace/myscripts/run.sh".execute().text.
 out.each{ if (it.contains(".tar.gz") ){
   if ( ! key.isEmpty()){ map[key]=lss; key=it; lss=[] } else { key=it } }
 else { lss.add('"'+it+'"') } } ; map[key]=lss
-return """ ${map[mypack]} """
+return """ return ${map[mypack]} """
 }
 def checkBuildRunning(){
     //Jenkins.instance.queue.clear()

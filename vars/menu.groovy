9cdf8ae30961e0de810392d4f1d716724cccba25
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-02 16:40:02 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -25,10 +25,11 @@ def getCompList(String pack){
     out.each{ var->
        println var
        if (var.contains(".tar.gz") ){
-          if ( ! lss.isEmpty()){
+          if ( ! key.isEmpty()){
             println "set map $key, $lss"
             map[key]=lss
             lss=[]
+            key=''
           } else { 
             key=var.split("\\.")[0]
             println "set key: $key"

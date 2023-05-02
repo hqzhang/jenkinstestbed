609026ec061d01e72d9741ae885840eded63084d
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-02 17:37:50 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -53,15 +53,15 @@ def getCompList(String mypack){
     | """.stripMargin()
 }
 def gitCompVerify(){
-def mypack='file.tar.gz'
+def package='file.tar.gz'
 def map=[:]
 def lss=[]
 def key=''
 def out="ssh root@192.168.0.16 /root/workspace/myscripts/run.sh".execute().text.readLines()
 out.each{ if (it.contains(".tar.gz") ){
   if ( ! key.isEmpty()){ map[key]=lss; key=it; lss=[] } else { key=it } }
-else { lss.add(it) } } ; map[key]=lss
-return """return ${map[mypack]} """
+else { lss.add("\"+it+\"") } } ; map[key]=lss
+return """return ${map[package]} """
 
 }
 def checkBuildRunning(){

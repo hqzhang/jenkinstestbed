8711e945f7efee075f79089fe1210166806df35c
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-02 22:11:41 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -59,7 +59,7 @@ def getCompList(String mypack){
     |def out="ssh root@192.168.0.16 /root/workspace/myscripts/run.sh".execute().text.readLines()
     |out.each{ if (it.contains(".tar.gz") ){
     |if ( ! key.isEmpty()){ map[key]=lss; key=it.split("\\\\.")[0]; lss=[] } else { key=it.split("\\\\.")[0] } }
-    |else { lss.add('"'+it+slt+'"') } } ; map[key]=lss
+    |else { lss.add(it+slt) } } ; map[key]=lss
     |println map
     |return map[mykey]
     | """.stripMargin()

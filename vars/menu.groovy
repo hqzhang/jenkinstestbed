981e3f1e11fe6b0f31eb94edc985658ac8df7b3c
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-05 11:37:20 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -122,8 +122,8 @@ def checkBuildRunning(){
             def job = Jenkins.instance.getItemByFullName(val[1].trim())
             for (build in job.builds) {
                 if (build.isBuilding()) {
-                    println (currentBuild)
-                    println(build)
+                    println ("currentbuild:",currentBuild)
+                    println("build:",build)
                     
                     //build.doStop();
                 }

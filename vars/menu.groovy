070ea78b55eaa231908b5b9d338b8ec62221e710
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-06-15 13:31:57 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -230,11 +230,9 @@ def getContentSimple(String refvar ){
 }
 def getContentSimpleVerify(){
     println "enter getContentSimpleverify()================74582375270=================="
-    def wksp=getWksp()
-    def repo=getRepo()
-    def brch=getBranch()
-    def Config='solution'
-    def out="cat /var/root/.jenkins/workspace/acgroovytest/release/${Config}.yaml".execute().text
+    def SolutionConfig='solution'
+    def out="cat /var/root/.jenkins/workspace/acgroovytest/release/${SolutionConfig}.yaml".execute().text
+    if (out.isEmpty()) { out="curl -k https://raw.githubusercontent.com/hqzhang/groovytest/mymenu/release/${SolutionConfig}.yaml".execute().text }
     out=out.replaceAll('components:\n','')
     return """ <textarea name="value"  value  class="setting-input  " type="text" rows="10" cols="25">${out}</textarea> """
 }

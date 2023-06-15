3dd99e303562e208b996f512705f94b4b8839e13
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-06-15 14:04:49 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -212,6 +212,7 @@ def getContent(String refvar ){
       |def urlext=\"${urlext}\"
       |//def map="curl -k \${url}/${repo}/${brch}/release/\${${refvar}}.yaml\$urlext".execute().text
       |def map="cat \${wksp}/release/\${${refvar}}.yaml".execute().text 
+      |map=map.replaceAll('components:\n','')
       |return \"\"\" <textarea name=\"value\"  value  class=\"setting-input  \" type=\"text\" rows="8" cols="25">\${map}</textarea> \"\"\"
       | """.stripMargin()
 }

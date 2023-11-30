9d41a6375366a0aa65fac53e7cfc7be8c2f12adf
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-11-29 20:44:54 -0500

    update

--- gittest/my.groovy
+++ gittest/my.groovy
@@ -21,25 +21,25 @@ def pwdCmd(){
 def deploy="""
 09:11:25 ... Deploying component Anvil_TradeEntry to /app/anvil/share/bin/Anvil_TradeEntry on 20.215.145.76
 """
-def download="""09:11:25 Files for Anv version-233 already present in /app/anvil/installer/rpository/Anv233"""
-def getKeys(String input){
+def download="""
+fsd  afa afa ...
+09:11:25 Files for Anv version-233 already present in /app/anvil/installer/repository/Anv233
+afafjaf aa
+"""
+def getKeys(String input,String key){
    def map=[:]
-   input.readLines().each { line->
-      def var=line.split()
-      def len=var.size()
-      for( i=0; i<len; i++){
-         for( j=0; j<len; j++){
-            if (var[i].length()>2 && var[j].contains('app') && i!=j && var[j].contains(var[i])) map[var[i]]=var[j]
-
+   def log=input.readLines().findAll{  it.contains(key)}
+   log.each { line->
+      if (line.contains(key)){
+         def path=line.split().find{ it.contains(key) }
+         line.split().each {
+            if(path.contains(it) && it.length()>2 && path!=it) map[it]=path
          }
       }
    }
    return map
 }
-data=download.split()
-searchTerm='Anv'
-def result = data.findAll { it.contains(searchTerm) }
-println "result=$result"
-println getKeys(download)
-println getKeys(deploy)
+
+println getKeys(download,'/repository/')
+
 

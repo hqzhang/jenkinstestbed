76444c1df4731899d5b8a0173235b0c445b13fd6
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-12-02 15:47:05 -0500

    update

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -26,3 +26,20 @@ def readYamlFileString(){
 }
 
 readYamlFileString()
+import groovy.lang.GroovyClassLoader
+
+def getClasspathEntryPath(Class clazz) {
+
+    def classLoader = clazz.classLoader
+    println GroovySystem.version
+    if (classLoader instanceof GroovyClassLoader) {
+        println classLoader.getURLs()
+        def classpath = classLoader.getURLs().find { it.path.endsWith("groovy-all-4.0.11.jar") } // Replace x.y.z with the version
+        return classpath ? classpath.path : null
+    } else {
+        return null
+    }
+}
+
+def groovyLibraryPath = getClasspathEntryPath(getClass())
+println "Groovy Library Path: $groovyLibraryPath"

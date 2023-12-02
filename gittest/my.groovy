03269b8684adf6159379ffe4e5de69b05a729c10
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-12-01 19:08:37 -0500

    update

--- gittest/my.groovy
+++ gittest/my.groovy
@@ -1,91 +1,22 @@
 
-
-def exeCmd(String cmd){
-    println cmd
-    proc=cmd.execute()
-    out=proc.text
-    code=proc.exitValue()
-    return out
-}
-
-def gitStatus(){
-   cmd ="git status "
-   return exeCmd(cmd)
-}
-
-def pwdCmd(){
-   cmd= "pwd"
-   return exeCmd(cmd)
-}
-
-def deploy="""
-09:11:25 ... Deploying component Anvil_TradeEntry to /app/anvil/share/bin/Anvil_TradeEntry on 20.215.145.76
-"""
-def download="""
-fsd  afa afa ...
-09:11:25 Files for Anv version-233 already present in /app/anvil/installer/repository/Anv233
-afafjaf aa
-"""
-def getKeys(String input,String key){
-   def map=[:]
-   def log=input.readLines().findAll{  it.contains(key)}
-   log.each { line->
-      if (line.contains(key)){
-         def path=line.split().find{ it.contains(key) }
-         line.split().each {
-            if(path.contains(it) && it.length()>2 && path!=it) map[it]=path
-         }
-      }
-   }
-   return map
-}
-
-def out=getKeys(deploy,'/bin/')
-def getBase(Map out){
-   def list=[]
-  out.each { type, mypath->
-      def index = mypath.lastIndexOf('/')
-      list.add( mypath[0..index-1])
-  }
-  return list
-}
-println getBase(out)
-
-def input = "abc1234"
-def separate(String input){
-   def map=[]
-   def match= input =~ /(\D+)(\d+)/
-   def name = (input =~ /\D+/).findAll()
-   def number= (input =~ /\d+/).findAll()
-   println name
-   println number
-   if (match) {
-      map.add(match[0][1])
-      map.add(match[0][2])
-   } else {
-      println "No match found"
-   }
-   return map
-}
-println separate(input)
-def originalString = "abc_def"
-def convertedString = originalString.toUpperCase()
-
-println(convertedString)
-@Grab('org.yaml:snakeyaml:1.29')
-import org.yaml.snakeyaml.Yaml
 import groovy.yaml.YamlSlurper
-
-
+println GroovySystem.version
 def data = """
 - !comp
    name: abc
 - !comp
    Name: elf
 """
-println GroovySystem.version
 
-def config = new YamlSlurper().parseText(data)
-config.each { entry ->
-    println(entry)
+def readYamlFileExt(String fileName){
+    String fileConts = new File(fileName).text
+    println ("-----------parsing-------")
+    def myyaml=new YamlSlurper()
+    return  myyaml.parseText(fileConts)
 }
+
+
+myfile="/Users/hongqizhang/workspace/groovytest/work/configuration.yml"
+println readYamlFileExt(myfile)
+def myyaml=new YamlSlurper()
+println  myyaml.parseText(data)

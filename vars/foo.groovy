1d1b9417fd9adbfb48ef58f77ca380b093fa6346
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-12-20 20:35:20 -0500

    update

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,21 +1,3 @@
-import groovy.yaml.YamlSlurper
-
-println GroovySystem.version
-
-def readYamlFileExt(String fileName){
-    String fileConts = new File(fileName).text
-    println ("-----------parsing-------")
-    def myyaml=new YamlSlurper()
-    return  myyaml.parseText(fileConts)
-}
-
-def readYamlFileString(){
-    withGroovy(tool:'4.0.11') {
-    sh """groovy --version
-        ./rungroovy.groovy 
-        """             
-    }
-}
 
 def justTest(){
     println "Enter justTest()"

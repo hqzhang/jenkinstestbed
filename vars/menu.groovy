01a08bf15e34b203519fda51b63f0a792c600275
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-02-07 17:12:28 -0500

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -436,6 +436,7 @@ def verify1(){
 def verify2(String SolutionConfig){
     println("enter verify2()....")
     def out="curl https://bitbucket.org/hqzhang/solution-repo/raw/master/${SolutionConfig}"
+    println "cmd=$out"
     out=out.execute().text
     out=out.replaceAll('components:\n','')
     return """ <textarea name="value"  value  class="setting-input  " type="text" rows="10" cols="25">${out}</textarea> """

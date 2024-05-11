cdfada6ce0de36627a6de4b9872e8a6d821200e5
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-05-11 13:54:21 -0400

    update

---
+++ src/com/wc/shlib/Class.groovy
@@ -0,0 +1,9 @@
+package com.wc.shlib;
+
+class Class{
+    static final String MAVEN = "Apache Maven 3.3.9";
+    static final String GITHUB_CREDENTIALS_ID = "";
+    static String sayHello(String str){
+        return "Hello, ${str}!"
+    }
+} 
---
+++ src/com/wc/shlib/Cons.groovy
@@ -0,0 +1,6 @@
+package com.wc.shlib;
+
+class Constants{
+    static final String MAVEN = "Apache Maven 3.3.9";
+    static final String GITHUB_CREDENTIALS_ID = "";
+}

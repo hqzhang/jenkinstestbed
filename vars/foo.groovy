3cdd16d8cee37420268a355d89c8949bf47c16c1
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-05-12 16:09:14 -0400

    update

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,10 +1,10 @@
-import com.wc.Class
-import com.wc.Constants
+//import com.wc.Class
+//import com.wc.Constants
 def call(String str){
-    def test =  new Class()
-    println Constants.GITHUB_CREDENTIALS_ID
-    println Class.MAVEN
-    println Class.sayHello(str)
+    def test =  new com.wc.Class()
+    //println Constants.GITHUB_CREDENTIALS_ID
+    //println Class.MAVEN
+    //println Class.sayHello(str)
     def ret = test.sayGoodbye(str)
     return ret
 }
